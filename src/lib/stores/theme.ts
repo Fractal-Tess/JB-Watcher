014e60260263f8f2c81f910e19e0ade90d747844
Fractal-Tess <vgfractal@gmail.com>
2022-08-02 02:43:07 +0300

    default theme

--- src/lib/stores/theme.ts
+++ src/lib/stores/theme.ts
@@ -1,29 +1,28 @@
 import { Store } from 'tauri-plugin-store-api';
 import type { Theme } from '$types';
-import { writable } from 'svelte/store';
+import { writable, type Writable } from 'svelte/store';
 
 const store = new Store('.settings.dat');
 
-const createThemeStore = () => {
-  const { subscribe, update, set } = writable('');
+const DEFAULT_THEME = 'dark';
 
-  return {
-    subscribe,
-    toggleTheme: () => {
-      update(currentTheme => {
-        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-        store.set('theme', newTheme);
-        return newTheme;
-      });
-    },
-    load: async () => {
-      let storedTheme = (await store.get('theme')) as Theme | null;
-      if (!storedTheme) {
-        storedTheme = 'dark';
-        store.set('theme', storedTheme);
-      }
-      set(storedTheme);
+const { subscribe, update, set } = writable() as Writable<string>;
+
+export const theme = {
+  subscribe,
+  toggleTheme: () => {
+    update(currentTheme => {
+      const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+      store.set('theme', newTheme);
+      return newTheme;
+    });
+  },
+  load: async () => {
+    let storedTheme = (await store.get('theme')) as Theme | null;
+    if (!storedTheme) {
+      storedTheme = DEFAULT_THEME;
+      store.set('theme', storedTheme);
     }
-  };
+    set(storedTheme);
+  }
 };
-export const theme = createThemeStore();
